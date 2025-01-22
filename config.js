b1977ac5099f978fd0fbaac8216540c03e039a60
Anixer1 <165531263+Anixer1@users.noreply.github.com>
2025-01-23 00:32:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
-const api = "https://streaming23-xysb.vercel.app/api/v2/hianime/home";  // Your deployed API URL
-const search = "https://streaming23-xysb.vercel.app/api/v2/hianime/search"; // Search API URL
-const details = "https://streaming23-xysb.vercel.app/api/v2/hianime/anime";  // Your additional variable
+const api = "https://streaming23-ap1i.vercel.app/api/v2/hianime/home";  // Your deployed API URL
+const search = "https://streaming23-ap1i.vercel.app/api/v2/hianime/search"; // Search API URL
+const details = "https://streaming23-ap1i.vercel.app/api/v2/hianime/anime";  // Your additional variable
 
 export default api;  // Change to default export
 export { search, details };  // Export search and anotherVariable as named exports
